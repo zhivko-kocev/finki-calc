60633fd19e748786aab6874cec27fa940be59716
zhivko-kocev <zikekocev57@gmail.com>
2024-09-01 17:42:27 +0200

    Initial commit

---
+++ finki-calc-frontend/src/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    try {
+      fetch("http://localhost:4040/api/subjects").then((res) =>
+        res.json().then((arr) => {
+          setData(arr);
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      {JSON.stringify(data)}
+    </>
+  );
+}
+
+export default App;
